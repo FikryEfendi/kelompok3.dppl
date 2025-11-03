1838ac7a511f4dba53cb8a6f364f4484d1a74017
FikryEfendi <fikry.efendi2336@gmail.com>
2025-11-03 09:47:46 +0700

    update main

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Belajar{
+public class Main{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean kondisi = true;
